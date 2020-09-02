3c285bf6f22a920ca268f988b076aebfeca15b2e
Cayce House <cayce@mobilemedics.com>
2020-09-02 17:59:21 -0400

    Writes username to file on thawspace

--- main.js
+++ main.js
@@ -21,13 +21,12 @@ function createWindow() {
   });
 
   mainWindow.setFullScreen(true);
-  mainWindow.setResizable(false);
 
   // and load the index.html of the app.
   mainWindow.loadFile("index.html");
 
   // Open the DevTools.
-  // mainWindow.webContents.openDevTools()
+  // mainWindow.webContents.openDevTools();
 }
 
 // This method will be called when Electron has finished
--- preload.js
+++ preload.js
@@ -3,9 +3,22 @@
 window.addEventListener('DOMContentLoaded', () => {
     // Retrieve remote BrowserWindow
     const { BrowserWindow } = require("electron").remote;
+    const username = require('username');
+    const os = require('os');
+    const fs = require('fs');
 
     // Close app
     document.getElementById("close-btn").addEventListener("click", e => {
+        // Log acceptance of policy.
+        let record = os.hostname() + " - " + username.sync() + " - " + new Date() + "\r\n";
+        console.log(record);
+
+        fs.writeFileSync("T:\\policy-banner.txt", record, { flag: 'a+' }, function (err) {
+            if (err) throw err;
+            console.log("It's saved!");
+        });
+
+        // Close application.
         var window = BrowserWindow.getFocusedWindow();
         window.close();
     });
